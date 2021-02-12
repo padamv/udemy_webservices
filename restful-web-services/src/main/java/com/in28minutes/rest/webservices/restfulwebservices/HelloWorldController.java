abffab7e1b4c5a85415da6fb4051b8758b53c209
padamv <padamv91@gmail.com>
2021-02-12 15:25:36 +0100

    Use PathVariable

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
@@ -21,6 +18,11 @@ public class HelloWorldController {
         return new HelloWorldBean("Hello World");
     }
 
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+
+    }
 
 
 }
