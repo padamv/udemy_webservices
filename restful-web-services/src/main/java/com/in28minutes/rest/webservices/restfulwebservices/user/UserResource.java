6b5440f0bb601275f2db0b7e003a126af0e74112
padamv <padamv91@gmail.com>
2021-02-13 09:33:20 +0100

    Add delete user functionality and map it to delete request

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -46,4 +46,12 @@ public class UserResource {
         return ResponseEntity.created(location).build();
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+    }
+
 }
