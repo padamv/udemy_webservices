0e45bd3bdb1dde0792f8bc0b6b893f4ae5cc5614
padamv <padamv91@gmail.com>
2021-02-13 11:30:28 +0100

    Simplify i18n implementation

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -31,8 +32,8 @@ public class HelloWorldController {
     }
 
     @GetMapping("/hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale){
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 
 
