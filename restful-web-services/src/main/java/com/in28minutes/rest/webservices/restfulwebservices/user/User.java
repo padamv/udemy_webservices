730e7a138e1185b832adffb6921dc0960349e6fc
padamv <padamv91@gmail.com>
2021-02-15 14:29:43 +0100

    Add custom api description with swagger

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,17 +1,23 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description="All details about the user.")
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes="Name should be at least 2 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes="Birth date should be in the past")
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
