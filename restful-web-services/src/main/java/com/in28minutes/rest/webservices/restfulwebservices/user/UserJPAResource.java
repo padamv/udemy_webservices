7b46fb08e342412a28180d8fec4d8ac9d9cf7336
padamv <padamv91@gmail.com>
2021-02-17 12:04:51 +0100

    Create delete user by id and create user REST functionality with JPA

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -16,9 +16,6 @@ import java.util.Optional;
 public class UserJPAResource {
 
     @Autowired
-    private UserDaoService service;
-
-    @Autowired
     private UserRepository userRepository;
 
     //retrieveAllUsers
@@ -52,7 +49,7 @@ public class UserJPAResource {
     // output - created & return the created Uri
     @PostMapping("/jpa/users")
     public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
-        User savedUser = service.save(user);
+        User savedUser = userRepository.save(user);
         //CREATED
         // user/{id}         savedUser.getId()
 
@@ -65,10 +62,8 @@ public class UserJPAResource {
 
     @DeleteMapping("/jpa/users/{id}")
     public void deleteUser(@PathVariable int id){
-        User user = service.deleteById(id);
-        if(user == null) {
-            throw new UserNotFoundException("id-" + id);
-        }
+        userRepository.deleteById(id);
+
     }
 
 }
