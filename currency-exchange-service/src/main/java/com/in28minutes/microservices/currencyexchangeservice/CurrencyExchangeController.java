221cf33c4001c7466c0362b40ebd9d591d9a59a5
padamv <padamv91@gmail.com>
2021-02-25 14:06:30 +0100

    Connection currency exchange microservice with zipkin

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository repository;
 
@@ -21,6 +25,8 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to){
+        logger.info("retrieveExhcangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
             throw new RuntimeException("Unable to Find data for " + from + " to " + to);
